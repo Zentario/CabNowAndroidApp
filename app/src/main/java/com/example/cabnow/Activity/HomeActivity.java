0e9c9caec4a38e119b001f6320accf5338cd0cda
Anirudh <anirudh@edaim.com>
2019-09-07 20:33:22 +0530

    current location updated

--- app/src/main/java/com/example/cabnow/Activity/HomeActivity.java
+++ app/src/main/java/com/example/cabnow/Activity/HomeActivity.java
@@ -1,14 +1,18 @@
 package com.example.cabnow.Activity;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.cabnow.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -20,86 +24,65 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.List;
 
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "HomeActivity";
-    private GoogleMap mMap;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private static final float DEFAULT_ZOOM = 15f;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location currentLocation;
+    private static final float DEFAULT_ZOOM = 16f;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        }
+        Log.d(TAG, "onCreate: started map activity");
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
 
+    }
+
+    private void getLastLocation() {
+        Task<Location> task = mFusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null)
+                {
+                    Log.d(TAG, "onSuccess: Current location at " + "Latitude : " + location.getLatitude() + " Longitude : " + location.getLongitude());
+                    currentLocation = location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    if (mapFragment != null) {
+                        mapFragment.getMapAsync(HomeActivity.this);
+                    }
+                }
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Log.d(TAG, "onMapReady: Map is ready");
 
-        mMap = googleMap;
-
-        getDeviceLocation();
-        mMap.setMyLocationEnabled(true);
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+        googleMap.setMyLocationEnabled(true);
     }
 
-    private void moveCamera(LatLng latLng, float zoom)
-    {
-        Log.d(TAG, "moveCamera: Moving the camera to " + latLng.latitude + " " + latLng.longitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
-    }
 
-    private void getDeviceLocation()
-    {
-        Log.d(TAG, "getDeviceLocation: Getting devices current location");
-
-        // CRASHES FROM BELOW
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        try {
-            Task<Location> location = fusedLocationProviderClient.getLastLocation();
-            location.addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    if(task.isSuccessful())
-                    {
-                        Log.d(TAG, "onComplete: Found location");
-                        Log.d(TAG, "onComplete: " + task.getResult());
-                        Location currentLocation = (Location) task.getResult();
-                        if(currentLocation != null)
-                        {
-                            Log.d(TAG, "onComplete: Location found at " + "Latitude : " + currentLocation.getLatitude()
-                             + " " + " Longitude : " + currentLocation.getLongitude());
-                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
-
-                        }
-                        else
-                            Toast.makeText(HomeActivity.this, "Current location is NULL", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(HomeActivity.this, "Cannot find your location", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-        catch(SecurityException e)
-        {
-            Log.e(TAG, "onMapReady: " + e.getMessage());
-        }
-
-    }
 
 }
